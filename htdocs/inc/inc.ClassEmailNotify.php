d1d69bbed6b5db4c961bd39df1c1efcb4654dc9b
Luis Medrano <lmedrano@multisistemas.com.sv>
2016-11-11 13:32:01 -0600

    fix send emails

--- htdocs/inc/inc.ClassEmailNotify.php
+++ htdocs/inc/inc.ClassEmailNotify.php
@@ -129,7 +129,7 @@ class SeedDMS_EmailNotify extends SeedDMS_Notify {
 			$mail->SMTPDebug = 0;
 			$mail->Debugoutput = 'html';
 			var_dump($mail);
-			var_export(expression)
+			var_export(expression);
 		} /*else {
 			$mail = Mail::factory('mail', $mail_params);
 		}*/
